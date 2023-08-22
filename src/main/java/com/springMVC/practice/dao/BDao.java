a52bae88dd895899c4680e3c020466fe9e1d31f8
NamSangwon <nsw3265@naver.com>
2023-08-22 18:22:29 +0900

    Build Command to get content list from DB

--- src/main/java/com/springMVC/practice/dao/BDao.java
+++ src/main/java/com/springMVC/practice/dao/BDao.java
@@ -1,4 +1,14 @@
 package com.springMVC.practice.dao;
 
+import com.springMVC.practice.dto.BDto;
+
+import java.util.ArrayList;
+
 public class BDao {
+    public ArrayList<BDto> list(){
+
+        ArrayList<BDto> dtos = null;
+
+        return dtos;
+    }
 }
