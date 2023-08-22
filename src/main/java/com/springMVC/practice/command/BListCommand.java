baf615732ca1510593a91dc8129f7681399f3df2
NamSangwon <nsw3265@naver.com>
2023-08-22 18:20:40 +0900

    Build List Command

--- src/main/java/com/springMVC/practice/command/BListCommand.java
+++ src/main/java/com/springMVC/practice/command/BListCommand.java
@@ -1,10 +1,17 @@
 package com.springMVC.practice.command;
 
+import com.springMVC.practice.dao.BDao;
+import com.springMVC.practice.dto.BDto;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
 
 public class BListCommand implements BCommand{
     @Override
     public void execute(Model model) {
-
+        // DAO에서 구성한 리스트 반환
+        BDao dao = new BDao();
+        ArrayList<BDto> dtos = dao.list();
+        model.addAttribute("list", dtos);
     }
 }
